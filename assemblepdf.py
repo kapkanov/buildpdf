244bfcd4fc4eac113e035058dd2de08e818cfb69
Anatoly Kapkanov <anatoly@kapkanov.com>
2024-09-27 22:32:08 +0400

    init

---
+++ assemblepdf.py
@@ -0,0 +1,90 @@
+# requirements.txt
+# pymupdf
+from   tkinter     import *
+from   tkinter     import ttk
+from   tkinter     import filedialog
+from   PIL         import ImageTk, Image
+import os
+import pymupdf
+import io
+
+
+buffer = []
+images = []
+pdfs   = []
+currow = 1
+
+root   = Tk()
+root.title("Assemble PDF")
+frame  = ttk.Frame(root)
+frame.pack()
+
+parent = frame
+
+
+def add_element(element):
+  element.pack()
+
+
+def reload_frame():
+  # for child in frame.winfo_children():
+  #   child.destroy()
+  # swin = ScrolledWindow(parent, width=500, height=500)
+  # swin.grid()
+  # win  = swin.window
+  for img in images:
+    load_image(img)
+
+
+def load_image(img):
+  img         = img.resize((300,300))
+  imgtk       = ImageTk.PhotoImage(img)
+  panel       = ttk.Label(parent, image=imgtk)
+  panel.image = imgtk
+  add_element(panel)
+
+
+def load_fimage(filename):
+  img         = Image.open(filename).resize((300,300))
+  img.resize((100,100))
+  imgtk       = ImageTk.PhotoImage(img)
+  panel       = ttk.Label(win, image=imgtk)
+  panel.image = imgtk
+  add_element(panel)
+
+
+def pdf2img(filename):
+  global buffer
+  img    = []
+  for page in pymupdf.open(filename):
+    pixmap = page.get_pixmap()
+    buffer.append(io.BytesIO(pixmap.tobytes()))
+    img.append(Image.open(buffer[-1]))
+  return img
+
+
+def open_file():
+  global images
+  filename = filedialog.askopenfilename(
+    title="Open file",
+    initialdir=os.path.dirname(os.path.realpath(__file__)),
+    filetypes=[("All files", "*.*")]
+  )
+  _, extension = os.path.splitext(filename)
+  if extension == ".pdf":
+    images = images + pdf2img(filename)
+  elif extension == ".jpg" \
+  or   extension == ".jpeg" \
+  or   extension == ".png" \
+  or   extension == ".gif" \
+  or   extension == ".bmp":
+    ttk.Label(parent, text=f"Image! {extension}").pack()
+  else:
+    ttk.Label(parent, text=f"Garbage! {extension}").pack()
+  reload_frame()
+
+
+button = ttk.Button(parent, text="+", command=open_file)
+add_element(button)
+
+root.mainloop()
